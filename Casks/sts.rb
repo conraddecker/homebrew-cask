3f5c5f46472d5b0461c130a6a0f2cc0e9db05134
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 02:17:38 +0000

    updated sts (3.6.3)

--- Casks/sts.rb
+++ Casks/sts.rb
@@ -1,14 +1,15 @@
 cask :v1 => 'sts' do
-  version '3.5.1'
-  sha256 'f71274c9f946d2af6bbd12e811d7c8d371d3031415839b9aa6ed35347d2980f8'
+  version '3.6.3'
+  sha256 '85b8e7a80460bfdffb19044ad789b7d1364bf82c00387f8bcfa8aff1f3140b49'
 
   module Utils
     def self.based_on_eclipse
-      '4.3.2'   # find eclipse version at http://spring.io/tools/sts/all
+      '4.4.1'   # find eclipse version at http://spring.io/tools/sts/all
     end
   end
 
-  url "http://download.springsource.com/release/STS/#{version}/dist/e#{Utils.based_on_eclipse.gsub(/\.\d$/, '')}/spring-tool-suite-#{version}.RELEASE-e#{Utils.based_on_eclipse}-macosx-cocoa-x86_64-installer.dmg"
+  # springsource.com is the official download host per the vendor homepage
+  url "http://download.springsource.com/release/STS/#{version}/dist/e#{Utils.based_on_eclipse.gsub(%r{\.\d$}, '')}/spring-tool-suite-#{version}.RELEASE-e#{Utils.based_on_eclipse}-macosx-cocoa-x86_64-installer.dmg"
   homepage 'http://spring.io/tools/sts'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
