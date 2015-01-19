08cde61b5bbe70f951f4fd166ca90994cb1df85c
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 02:14:43 +0000

    updated tabula (0.9.6)

--- Casks/tabula.rb
+++ Casks/tabula.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'tabula' do
-  version '0.9.5.2'
-  sha256 'b6a2663cbf440d27cc94ac9ac05ac1875136e81f63980a25e1acaf6a3250e131'
+  version '0.9.6'
+  sha256 '85b8e7a80460bfdffb19044ad789b7d1364bf82c00387f8bcfa8aff1f3140b49'
 
-  url "https://github.com/jazzido/tabula/releases/download/v0.9.5/tabula-mac-#{version}.zip"
+  # github.com is the official download host per the vendor homepage
+  url "https://github.com/tabulapdf/tabula/releases/download/v#{version}/tabula-mac-#{version}.zip"
   homepage 'http://tabula.nerdpower.org'
   license :mit
 
