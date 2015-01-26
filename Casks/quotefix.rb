10c49a01ee4c7eedf7a729b0c1f14c5a0a0e6e3a
Vítor Galvão <info@vitorgalvao.com>
2015-01-26 16:24:18 +0000

    quotefix.rb: removed 'formac' from name

--- Casks/quotefix.rb
+++ Casks/quotefix.rb
@@ -1,4 +1,4 @@
-cask :v1 => 'quotefixformac' do
+cask :v1 => 'quotefix' do
   version '2.5.2'
   sha256 '8d914ae553b84fe5f246ab1eb030d25792ca8d626b3bbac57acee857135e85a9'
 
@@ -6,7 +6,7 @@ cask :v1 => 'quotefixformac' do
   homepage 'https://github.com/robertklep/quotefixformac'
   license :oss
 
-  artifact 'QuoteFix.mailbundle', :target => Pathname.new(File.expand_path('~')).join('/Library/Mail/Bundles/QuoteFix.mailbundle')
+  artifact 'QuoteFix.mailbundle', :target => Pathname.new(File.expand_path('~')).join('Library/Mail/Bundles/QuoteFix.mailbundle')
   
   postflight do
     system 'defaults', 'write', 'com.apple.mail', 'EnableBundles', '-bool', 'true'
